7e518cce4ab1c4a0d62d6a218d6676add31a3ac3
berialx <berialx>
2023-07-03 17:05:42 +0000

    task one edited

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(idslist) {
     if (Array.isArray(idslist)) {
-        return idslist.map((obj) => obj.id)
+        return idslist.map((obj) => obj.id);
     }
 
     return [];
